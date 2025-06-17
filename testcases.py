7897e91f84f2fc07f252d3299de29acb839f921b
Asjad Ilahi <160521387+Asjad-Ilahi@users.noreply.github.com>
2025-06-18 02:24:08 +0500

    done

--- testcases.py
+++ testcases.py
@@ -9,7 +9,6 @@ def setup_browser():
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.binary_location = "/usr/bin/google-chrome"
     return webdriver.Chrome(options=options)
 
 
